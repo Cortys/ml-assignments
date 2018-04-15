84c6fdd6111255e57e98f448658f22553802ac5f
Clemens Damke <admin@cortys.de>
2018-04-15 20:54:04 +0200

    added assignment1

--- ml2/assignment1/src/task1.clj
+++ ml2/assignment1/src/task1.clj
@@ -5,13 +5,6 @@
             [clojure.data.csv :refer [read-csv]]
             [lambda-ml.regression :as reg]))
 
-(def α 0.1)
-(def λ 0.1)
-(def i 500)
-
-(def classes (range 10))
-(def data (delay (read-data "pendigits.tra")))
-
 (defn read-data
   [path]
   (let [raw-data (-> path resource reader read-csv)]
@@ -21,9 +14,12 @@
               :y (Integer/parseInt (str/trim y))}))
          raw-data)))
 
-(def random-data-splits (repeatedly #(let [data @data]
-                                       (split-at (/ (count data) 2)
-                                                 (shuffle data)))))
+(def α 0.1)
+(def λ 0.1)
+(def i 1)
+
+(def classes (range 10))
+(def data (delay (read-data "pendigits.tra")))
 
 (defn decompose
   [data classifier]
@@ -51,11 +47,65 @@
 (defn one-vs-rest-models
   [data classes]
   (->> classes
-       (map (juxt identity (comp log-model (partial one-vs-rest-decompose data))))
+       (pmap (juxt identity (comp log-model (partial one-vs-rest-decompose data))))
        (into {})))
 
 (defn all-pairs-models
   [data classes]
   (->> (combo/combinations classes 2)
-       (map (juxt identity (comp log-model (partial all-pairs-decompose data))))
+       (pmap (juxt identity (comp log-model (partial all-pairs-decompose data))))
        (into {})))
+
+(defn one-vs-rest-predict
+  [models x]
+  (apply max-key
+         #(first (reg/regression-predict (models %) [x]))
+         (keys models)))
+
+(defn all-pairs-predict
+  [models x]
+  (let [counts (reduce (fn [counts [c0 c1 :as k]]
+                         (let [c1-count (first (reg/regression-predict (models k) [x]))
+                               c0-count (- 1 c1-count)
+                               add (fnil + 0)]
+                           (-> counts
+                               (update c0 add c0-count)
+                               (update c1 add c1-count))))
+                       {} (keys models))]
+    (apply max-key counts (keys counts))))
+
+(defn accuracy
+  [predictor data]
+  (/ (count (keep (comp (partial apply =)
+                        (juxt (comp predictor :x) :y))
+                  data))
+     (count data)))
+
+(defn one-vs-rest-accuracy
+  [models data]
+  (accuracy (partial one-vs-rest-predict models) data))
+
+(defn all-pairs-accuracy
+  [models data]
+  (accuracy (partial all-pairs-predict models) data))
+
+(defn one-vs-rest-eval
+  [classes train test]
+  (one-vs-rest-accuracy (one-vs-rest-models train classes)
+                        test))
+
+(defn all-pairs-eval
+  [classes train test]
+  (all-pairs-accuracy (all-pairs-models train classes)
+                      test))
+
+(defn comparison
+  [classes data n]
+  (let [data-splits (repeatedly n #(split-at (/ (count data) 2)
+                                             (shuffle data)))]
+    (pmap (fn [i [train test]]
+            (let [ovr (one-vs-rest-eval classes train test)
+                  ap (all-pairs-eval classes train test)]
+              (println "Split " i ": OVR=" ovr ", AP=" ap)
+              {:ovr ovr, :ap ap}))
+          (range) data-splits)))
